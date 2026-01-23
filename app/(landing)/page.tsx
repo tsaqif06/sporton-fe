d70db0c1344c5cff02a398964933f1fc9dc711c8
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-01-23 10:12:50 +0700

    feat: add categories section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,9 @@
+import CategoriesSection from "./components/home/categories";
 import HeroSection from "./components/home/hero";
 
 export default function Home() {
   return <main>
     <HeroSection />
+    <CategoriesSection/>
   </main>;
 }
