1bedfed37acbe53c6bdc143d9489615c9ddc987a
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-01-23 10:41:19 +0700

    feat: add footer

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../globals.css";
 import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <Header/>
         {children}
+        <Footer />
       </body>
     </html>
   );
