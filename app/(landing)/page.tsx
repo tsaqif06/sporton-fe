4b37313605294a198445b4d4fe55889111a9e3fe
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-01-23 10:30:47 +0700

    feat: add products section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,11 @@
-import CategoriesSection from "./components/home/categories";
 import HeroSection from "./components/home/hero";
+import CategoriesSection from "./components/home/categories";
+import ProductsSection from "./components/home/products";
 
 export default function Home() {
   return <main>
     <HeroSection />
-    <CategoriesSection/>
+    <CategoriesSection />
+    <ProductsSection />
   </main>;
 }
