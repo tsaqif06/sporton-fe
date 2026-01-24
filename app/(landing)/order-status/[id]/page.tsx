08da21f8d6a09cfc203f46f594034a472d172552
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-01-24 21:15:39 +0700

    fix: fix responsiveness issue in order status page

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -8,10 +8,11 @@ const OrderStatus = () => {
   const [isConfirmed, setIsConfirmed] = useState(false);
 
   return (
-    <main className="bg-gray-100 min-h-[100vh]">
+    <main className="bg-gray-100 min-h-screen grid grid-rows-[auto_1fr]">
       <div className="max-w-5xl mx-auto py-20">
         <h1 className="text-5xl font-bold text-center mb-2">Order Status</h1>
       </div>
+
       <div className="mb-20">
         {isConfirmed ? <OrderConfirmed /> : <OrderSubmitted />}
       </div>
