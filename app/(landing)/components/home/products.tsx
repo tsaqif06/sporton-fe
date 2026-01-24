37d466c9fc239c9414825013d5e9ca75277fa070
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-01-24 11:17:27 +0700

    feat: add product detail page and popup cart ui

--- app/(landing)/components/home/products.tsx
+++ app/(landing)/components/home/products.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Button from "../ui/button";
 import { FiPlus } from "react-icons/fi";
+import priceFormatter from "@/app/utils/price-formatter";
 
 const productList = [
   {
@@ -71,11 +72,7 @@ const ProductsSection = () => {
             <div className="flex justify-between mb-8">
               <div className="text-gray-500">{product.category}</div>
               <div className="font-medium text-primary">
-                {Intl.NumberFormat("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                  maximumSignificantDigits: 3,
-                }).format(product.price)}
+                {priceFormatter(product.price)}
               </div>
             </div>
           </Link>
--- app/(landing)/components/layouts/header.tsx
+++ app/(landing)/components/layouts/header.tsx
@@ -1,29 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { FiSearch, FiShoppingBag } from "react-icons/fi";
+import CartPopup from "../ui/cart-popup";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-        <header>
-            <div className="flex justify-between gap-10 container mx-auto py-7 items-center">
-                <Image src="/images/logo.svg" alt="Logo" width={127} height={30} />
-                <nav className="flex gap-44 font-medium">
-                    <Link href="#" className="relative after:content-[''] after:block after:bg-primary after:rounded-full after:h-[3px] after:w-1/2 after:absolute after:left-1/2 after:-translate-x-1/2 after:translate-y-1">Home</Link>
-                    <Link href="#">Category</Link>
-                    <Link href="#">Explore Products</Link>
-                    <div className="flex gap-10">
-                        <FiSearch size={24} />
-                        <div className="relative">
-                            <FiShoppingBag size={24} />
-                            <div className="bg-primary rounded-full w-3.5 h-3.5 absolute -top-1 -right-1 text-[10px] text-white text-center">
-                                3
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    );
+  const [isCartPopupOpen, setIsCartPopupOpen] = useState(false);
+
+  return (
+    <header>
+      <div className="flex justify-between gap-10 container mx-auto py-7 items-center">
+        <Image src="/images/logo.svg" alt="Logo" width={127} height={30} />
+        <nav className="flex gap-44 font-medium">
+          <Link
+            href="#"
+            className="relative after:content-[''] after:block after:bg-primary after:rounded-full after:h-[3px] after:w-1/2 after:absolute after:left-1/2 after:-translate-x-1/2 after:translate-y-1"
+          >
+            Home
+          </Link>
+          <Link href="#">Category</Link>
+          <Link href="#">Explore Products</Link>
+          <div className="relative flex gap-10">
+            <FiSearch size={24} />
+            <button
+              className="relative cursor-pointer"
+              onClick={() => setIsCartPopupOpen(!isCartPopupOpen)}
+            >
+              <FiShoppingBag size={24} />
+              <div className="bg-primary rounded-full w-3.5 h-3.5 absolute -top-1 -right-1 text-[10px] text-white text-center">
+                3
+              </div>
+            </button>
+            {isCartPopupOpen && <CartPopup />}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
