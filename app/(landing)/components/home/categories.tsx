b6b7d89d2ff2f69acfb093c3132e6278a6c1f780
Tsaqif06 <ahmadtsaqifal6@gmail.com>
2026-01-25 09:15:29 +0700

    feat: implement category fetching and display on homepage

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -1,35 +1,41 @@
+import { getImageUrl } from "@/app/lib/api";
+import { Category } from "@/app/types";
 import Image from "next/image";
 import Link from "next/link";
 import { FiArrowRight } from "react-icons/fi";
 
-const categoryList = [
-  {
-    name: "Running",
-    imgUrl: "category-running.png",
-  },
-  {
-    name: "Tennis",
-    imgUrl: "category-tennis.png",
-  },
-  {
-    name: "Basketball",
-    imgUrl: "category-basketball.png",
-  },
-  {
-    name: "Football",
-    imgUrl: "category-football.png",
-  },
-  {
-    name: "Badminton",
-    imgUrl: "category-badminton.png",
-  },
-  {
-    name: "Swimming",
-    imgUrl: "category-swimming.png",
-  },
-];
+// const categoryList = [
+//   {
+//     name: "Running",
+//     imgUrl: "category-running.png",
+//   },
+//   {
+//     name: "Tennis",
+//     imgUrl: "category-tennis.png",
+//   },
+//   {
+//     name: "Basketball",
+//     imgUrl: "category-basketball.png",
+//   },
+//   {
+//     name: "Football",
+//     imgUrl: "category-football.png",
+//   },
+//   {
+//     name: "Badminton",
+//     imgUrl: "category-badminton.png",
+//   },
+//   {
+//     name: "Swimming",
+//     imgUrl: "category-swimming.png",
+//   },
+// ];
 
-const CategoriesSection = () => {
+type TCategoriesProps = {
+  categories: Category[];
+}
+
+const CategoriesSection = ({categories}: TCategoriesProps ) => {
   return (
     <section
       id="category-section"
@@ -47,14 +53,14 @@ const CategoriesSection = () => {
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 lg:gap-12 mt-8">
-        {categoryList.map((category, index) => (
+        {categories.map((category) => (
           <div
             className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex flex-col justify-center items-center p-4 transition-transform hover:scale-105 cursor-pointer"
-            key={index}
+            key={category._id}
           >
             <div className="relative w-16 h-16 md:w-20 md:h-20 mb-3">
               <Image
-                src={`/images/categories/${category.imgUrl}`}
+                src={getImageUrl(category.imageUrl)}
                 fill
                 alt={category.name}
                 className="object-contain"
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "./components/home/hero";
 import CategoriesSection from "./components/home/categories";
 import ProductsSection from "./components/home/products";
+import { getAllCategories } from "../services/category.service";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await getAllCategories();
+
   return <main>
     <HeroSection />
-    <CategoriesSection />
+    <CategoriesSection categories={categories} />
     <ProductsSection />
   </main>;
 }
